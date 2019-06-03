21aa6eb2aea83d75b2a527343a580ba133e66261
PengjuCao <45583724+PengjuCao@users.noreply.github.com>
2019-06-03 11:36:05 +0800

    Update EurekaServerApplication.java

--- eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -9,6 +9,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 //提交成功
 	//二次提交成功
+	//三次提交成功
+	//四次提交成功
+	//五次提交成功
+	//六次提交成功
+	//七次提交成功
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
