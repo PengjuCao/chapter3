1d33ace28565c22801370c2b5bfea2a051520251
PengjuCao <18737008789@163.com>
2019-06-03 11:05:20 +0800

    二次提交成功！

--- eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
-
+//提交成功
+	//二次提交成功
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
